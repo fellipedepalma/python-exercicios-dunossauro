f11c55e8390a00004a6d0a553220068abf530c89
Fellipe de Palma <fellipedepalma@gmail.com>
2025-08-14 10:11:31 -0300

    Update solucao.py

--- exercicios/009/solucao.py
+++ exercicios/009/solucao.py
@@ -1,3 +1,3 @@
 fahrenheit = float(input('Digite a temperatura em Fahrenheit: '))
 celsius = 5 * ((fahrenheit - 32) / 9)
-print(f'{fahrenheit}Fº são {celsius}Cº')
+print(f'{fahrenheit}ºF são {celsius}ºC')
