284348973b8fd5699d1c549f75411d0207888313
Fellipe de Palma <fellipedepalma@gmail.com>
2025-08-14 10:39:14 -0300

    Create solucao.py

---
+++ exercicios/011/solucao.py
@@ -0,0 +1,7 @@
+num_inteiro1 = int(input('Digite um número inteiro: '))
+num_inteiro2 = int(input('Digite outro número inteiro: '))
+num_real = float(input('Digite um número real: '))
+
+print(f'O produto do dobro do primeiro com metade do segundo é: {(num_inteiro1 ** 2) * (num_inteiro2 / 2)}')
+print(f'A soma do triplo do primeiro com o terceiro número é: {(num_inteiro1 * 3) + num_real}')
+print(f'O terceiro número elevado ao cubo é: {num_real ** 3}')
