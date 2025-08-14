c3cd7aaf90ce634e29c539b452297031a3806643
Fellipe de Palma <fellipedepalma@gmail.com>
2025-08-14 00:18:02 -0300

    Create solucao.py

---
+++ exercicios/008/solucao.py
@@ -0,0 +1,4 @@
+valor_hora = float(input('Quanto você recebe por hora: '))
+horas_trabalhadas = float(input('Digite o seu número de horas trabalhadas esse mês: '))
+salario_mes = valor_hora * horas_trabalhadas
+print(f'O seu salário mẽs foi: {salario_mes}')
